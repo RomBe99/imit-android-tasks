1da689e329f077548271b794dc10676f43ab0992
Belinsky Roman <romanbelinsky99@gmail.com>
2020-12-16 23:50:29 +0600

    Complete task 2, 3

--- app/src/main/java/ru/omsu/imit/androidtasks/MainActivity.kt
+++ app/src/main/java/ru/omsu/imit/androidtasks/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.omsu.imit.androidtasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var crowCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mysteryCatImageButton.setOnClickListener {
+            greetingTextView.text = getString(R.string.cat_greeting).format("stranger")
+        }
+
+        crowCountButton.setOnClickListener {
+            crowCountTextView.text = "Я насчитал ${++crowCount} Ворон"
+        }
     }
 }
